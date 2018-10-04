a787e44e2b1bdb1cadaf6742c8df0374f844e57c
Mohit Singh Chouhan <mohitchouhan1947@gmail.com>
2018-10-04 21:52:17 +0530

    Add files via upload

---
+++ newpyserial.ino
@@ -0,0 +1,23 @@
+#include<Servo.h>
+Servo lid;
+int analogPin=3;
+int servoPin=9;
+int data=0;
+char userInput;
+
+void setup() {
+Serial.begin(9600);
+lid.attach(servoPin);
+}
+
+void loop() {
+  if(Serial.available()>0){
+    userInput=Serial.read();
+    if(userInput=='g'){
+      lid.write(30);
+      data=analogRead(analogPin);
+      Serial.println(data);
+    }
+    
+  }
+}
